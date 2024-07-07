96680779ee58eb2ee0f94c46f1f5c39ae7dec030
Waptech <abdulfataiyakub07@gmail.com>
2024-07-07 06:37:56 +0100

    registered models in admin.py and customized the admin interface

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User, Organisation
 
 # Register your models here.
+class UserAdmin(BaseUserAdmin):
+    list_display = ('email', 'first_name', 'last_name', 'is_staff', 'is_superuser')
+    list_filter = ('is_staff', 'is_superuser', 'is_active')
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'phone')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser')}),
+    )
+    
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'first_name', 'last_name', 'password1', 'password2', 'is_staff', 'is_superuser'),
+        }),
+    )
+    search_fields = ('email', 'first_name', 'last_name')
+    ordering = ('email',)
+    filter_horizontal = ()
+
+class OrganisationAdmin(admin.ModelAdmin):
+    list_display = ('orgId', 'name')
+    search_fields = ('orgId', 'name')
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Organisation, OrganisationAdmin)
