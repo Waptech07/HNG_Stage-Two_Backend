a26f5a7d2c6665f23fa84c63096ca6707c6db678
Waptech <abdulfataiyakub07@gmail.com>
2024-07-07 20:29:04 +0100

    add endpoint urls for the views

---
+++ organisation/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import UserDetailView, OrganisationDetailView, OrganisationListCreateView, AddUserToOrganisationView
+
+urlpatterns = [
+    path('users/<int:id>/', UserDetailView.as_view(), name='user-detail'),
+    path('organisations/', OrganisationListCreateView.as_view(), name='organisation-list-create'),
+    path('organisations/<str:orgId>/', OrganisationDetailView.as_view(), name='organisation-detail'),
+    path('organisations/<str:orgId>/users/', AddUserToOrganisationView.as_view(), name='add-user-to-organisation'),
+]
