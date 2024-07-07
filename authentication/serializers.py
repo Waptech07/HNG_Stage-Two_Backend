d7d7dd45da871a23ed885880aac89af3801b8d29
Waptech <abdulfataiyakub07@gmail.com>
2024-07-07 06:55:41 +0100

    added phone field to the user serializer, made few changes

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -1,33 +1,21 @@
 from rest_framework import serializers
 from .models import User, Organisation
 from django.contrib.auth.hashers import make_password
-
+import uuid
 
 class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
         fields = ['userId', 'first_name', 'last_name', 'email', 'password', 'phone']
-        extra_kwargs = {'password': {'write_only': True}}
-
-    def validate(self, data):
-        errors = {}
-        if not data.get('first_name'):
-            errors['first_name'] = 'First name is required'
-        if not data.get('last_name'):
-            errors['last_name'] = 'Last name is required'
-        if not data.get('email'):
-            errors['email'] = 'Email is required'
-        if not data.get('password'):
-            errors['password'] = 'Password is required'
-        if User.objects.filter(email=data.get('email')).exists():
-            errors['email'] = 'User with this email already exists'
-        if errors:
-            raise serializers.ValidationError(errors)
-        return data
+        extra_kwargs = {
+            'password': {'write_only': True},
+            'userId': {'read_only': True}
+        }
 
     def create(self, validated_data):
         validated_data['password'] = make_password(validated_data['password'])
+        validated_data['userId'] = str(uuid.uuid4())
         return super(UserSerializer, self).create(validated_data)
 
 
